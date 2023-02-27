00e44a06fa53bb3d00881656947e796ef863eb7d
Matsebe Makhura <mmakhura@emea.corp.jwt.com>
2023-02-27 14:27:42 +0200

    modal component

--- app/src/pages/ModalPage.js
+++ app/src/pages/ModalPage.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
-import Modal  from "../components/Modal";
+import Modal from "../components/Modal";
 import Button from "../components/Button";
 
-function ModalPage(){
-    const [showModal,setShowModal] = useState(false);
+function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
 
-    const handleShowModal = () =>{
+    const handleShowModal = () => {
         setShowModal(!showModal)
     }
-
+    const closeModal = () => {
+        setShowModal(false)
+    }
+    const actionBar = <Button primary onClick={closeModal}>accept</Button>
+    const modal = <Modal onClose={closeModal} actionBar={actionBar}>
+        <p>paragraph chldren representing the content passed to the modal</p> </Modal>
     return <div>
         <Button secondary outline onClick={handleShowModal}> Open Modal</Button>
-        {showModal && <Modal />}
+        {showModal && modal}
     </div>
 }
 
