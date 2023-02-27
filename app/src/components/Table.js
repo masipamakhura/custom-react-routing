c7ed17a83117d3c9ab33464f40b50a21c6cfddf5
Matsebe Makhura <mmakhura@emea.corp.jwt.com>
2023-02-27 16:30:10 +0200

    table ui and config files completed

---
+++ app/src/components/Table.js
@@ -0,0 +1,30 @@
+
+function Table({ data,config }) {
+    
+
+const tableHeading = config.map(heading =>{
+    return <th key={heading.label}>{heading.label}</th>
+})
+    const content = data.map((data,i) => {
+        return <tr className="border-b" key={data.name}>
+            {
+                config.map(conf =>{
+                    return   <td className="p-3" key={conf.label}>{conf.render(data)}</td>
+                })
+            }
+            
+            </tr>
+    })
+    return <table className="table-auto border-spacing-2 ">
+        <thead>
+            <tr className="border-b-2">
+                {tableHeading}
+            </tr>
+        </thead>
+        <tbody>
+            {content}
+        </tbody>
+    </table>
+}
+
+export default Table;
